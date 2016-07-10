abbd881ab7804ab5c13f4919e7902f6d510e92ac
Jason Miller <jason@developit.ca>
2016-07-10 17:31:39 -0400

    Make `undom/register` decorate `global` with all `window` properties, including `window` itself.

--- register.js
+++ register.js
@@ -1,1 +1,4 @@
-if (typeof global!=='undefined' && !global.document) global.document = require('.')();
+if (typeof global!=='undefined' && !global.document) {
+	global.window = require('.').defaultView;
+	for (var i in global.window) global[i] = global.window[i];
+}
