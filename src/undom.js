8e2cd74abaa1bcaa81292db80cec9d79a9399868
Jason Miller <developit@google.com>
2019-02-01 17:00:23 -0500

    Drop 21 bytes by moving utility methods into the environment closure

--- src/undom.js
+++ src/undom.js
@@ -1,11 +1,3 @@
-import {
-	assign,
-	toLower,
-	splice,
-	findWhere,
-	createAttributeFilter
-} from './util';
-
 /*
 const NODE_TYPES = {
 	ELEMENT_NODE: 1,
@@ -20,6 +12,29 @@ const NODE_TYPES = {
 */
 
 function createEnvironment() {
+	function assign(obj, props) {
+		for (let i in props) obj[i] = props[i];
+	}
+
+	function toLower(str) {
+		return String(str).toLowerCase();
+	}
+
+	function splice(arr, item, add, byValue) {
+		let i = arr ? findWhere(arr, item, true, byValue) : -1;
+		if (~i) add ? arr.splice(i, 0, add) : arr.splice(i, 1);
+		return i;
+	}
+
+	function findWhere(arr, fn, returnIndex, byValue) {
+		let i = arr.length;
+		while (i--) if (byValue ? arr[i]===fn : fn(arr[i])) break;
+		return returnIndex ? i : arr[i];
+	}
+
+	function createAttributeFilter(ns, name) {
+		return o => o.ns===ns && toLower(o.name)===toLower(name);
+	}
 
 function isElement(node) {
 	return node.nodeType===1;
